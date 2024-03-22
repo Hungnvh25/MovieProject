fc88bd06affc97d63e9fed9488974e656a0224d6
NguyenVanHung <hungnvh25@gmail.com>
2024-03-22 20:03:05 +0700

    add

--- MovieProject/src/frontend/vue.config.js
+++ MovieProject/src/frontend/vue.config.js
@@ -6,13 +6,13 @@ module.exports = defineConfig({
 module.exports = {
   // https://cli.vuejs.org/config/#devserver-proxy
   devServer: {
-      port: 3000,
-      proxy: {
-          '/api': {
-              target: 'http://localhost:8080',
-              ws: true,
-              changeOrigin: true
-          }
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
       }
+    }
   }
 }
--- MovieProject/src/main/java/com/movieproject/movieproject/MovieProjectApplication.java
+++ MovieProject/src/main/java/com/movieproject/movieproject/MovieProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieProjectApplication {
+public class movieprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieProjectApplication.class, args);
+		SpringApplication.run(movieprojectApplication.class, args);
 	}
 
 }
