1d43773955620d8ca9c8c257f3266ddfb62696ab
minhhieu111 <leminhhieu12112003@gmail.com>
2024-04-21 09:38:24 +0700

    m

--- Movie_Nodejs_Vuejs/view/src/pages/admin/TablePage.vue
+++ Movie_Nodejs_Vuejs/view/src/pages/admin/TablePage.vue
@@ -4,234 +4,58 @@
             <div></div>
             <p>{{this.$route.params.table_name.toUpperCase()+' Table'}}</p>
             <button>Add</button>
-        </div>
+        </div>      
+
+        <DynamicTable
+        :entity-type="'movies'"
+        :headers="['Id', 'Name', 'Title', 'Image','Video','View','Like','Description','Year']"
+        :data="tables"
+        v-if="$route.params.table_name === 'movies'"
+        />
+
+        <DynamicTable
+        :entity-type="'users'"
+        :headers="['Id', 'Username', 'Password', 'Email','Phone']"
+        :data="tables"
+        v-if="$route.params.table_name === 'users'"
+        />
+
+        <DynamicTable
+        :entity-type="'mylists'"
+        :headers="['Id', 'Name', 'User_id', 'Movie_id']"
+        :data="tables"
+        v-if="$route.params.table_name === 'mylists'"
+        />
+
+        <DynamicTable
+        :entity-type="'actors'"
+        :headers="['Id', 'Name', 'Movie_id']"
+        :data="tables"
+        v-if="$route.params.table_name === 'actors'"
+        />
+
+        <DynamicTable
+        :entity-type="'countries'"
+        :headers="['Id', 'Name', 'Movie_id']"
+        :data="tables"
+        v-if="$route.params.table_name === 'countries'"
+        />
+
+        <DynamicTable
+        :entity-type="'genres'"
+        :headers="['Id', 'Name', 'Movie_id']"
+        :data="tables"
+        v-if="$route.params.table_name === 'genres'"
+        />
+
+        <DynamicTable
+        :entity-type="'comments'"
+        :headers="['Id', 'Text', 'User_id', 'Movie_id']"
+        :data="tables"
+        v-if="$route.params.table_name === 'comments'"
+        />
+
         
-        
-        <!-- Movies -->
-        <table v-if="this.$route.params.table_name == 'movies' ">
-            <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Title</th>
-                    <th>Image</th>
-                    <th>Video</th>
-                    <th>View</th>
-                    <th>Like</th>
-                    <th>Description</th>
-                    <th>Year</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.name }}</td>
-                    <td>{{ table.title }}</td>
-                    <td>{{ table.image }}</td>
-                    <td>{{ table.video }}</td>
-                    <td>{{ table.view }}</td>
-                    <td>{{ table.like }}</td>
-                    <td>{{ table.description }}</td>
-                    <td>{{ table.year }}</td>
-                <td>
-
-                        <button class="edit-button">Edit</button>
-                    
-                        <button class="delete-button">Delete</button>
-                    
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="9">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Users -->
-        <table v-if="this.$route.params.table_name == 'users' ">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Username</th>
-                    <th>Password</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.username }}</td>
-                    <td>{{ table.password }}</td>
-                    <td>{{ table.email }}</td>
-                    <td>{{ table.phone }}</td>
-                <td>
-                        <button class="edit-button">Edit</button>
-                        <button class="delete-button">Delete</button>
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="5">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- My List -->
-        <table v-if="this.$route.params.table_name == 'mylists' ">
-            <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>userId</th>
-                    <th>movieId</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.name }}</td>
-                    <td>{{ table.userId }}</td>
-                    <td>{{ table.movieId }}</td>
-                <td>
-                        <button class="edit-button">Edit</button> 
-                        <button class="delete-button">Delete</button>
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="4">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Actors -->
-        <table v-if="this.$route.params.table_name == 'actors' ">
-            <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>movieId</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.name }}</td>
-                    <td>{{ table.movieId }}</td>
-                <td>
-                        <button class="edit-button">Edit</button>
-                        <button class="delete-button">Delete</button>
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="3">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Country -->
-        <table v-if="this.$route.params.table_name == 'countries' ">
-            <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>movieId</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.name }}</td>
-                    <td>{{ table.movieId }}</td>
-                <td>
-                        <button class="edit-button">Edit</button>
-                        <button class="delete-button">Delete</button>
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="3">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Genres -->
-        <table v-if="this.$route.params.table_name == 'genres' ">
-            <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>movieId</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.name }}</td>
-                    <td>{{ table.movieId }}</td>
-                <td>
-                        <button class="edit-button">Edit</button>
-                        <button class="delete-button">Delete</button>
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="3">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Comments -->
-        <table v-if="this.$route.params.table_name == 'comments' ">
-            <thead>
-                <tr >
-                    <th>Id</th>
-                    <th>Text</th>
-                    <th>UserId</th>
-                    <th>movieId</th>
-                    <th>Activity</th>
-                </tr>
-            </thead>
-            
-            <tbody v-if="tables">
-                <tr v-for="table in tables" :key="table.id">
-                    <td>{{ table.id }}</td>
-                    <td>{{ table.text }}</td>
-                    <td>{{ table.userId }}</td>
-                    <td>{{ table.movieId }}</td>
-                <td>
-                        <button class="edit-button">Edit</button>
-                        <button class="delete-button">Delete</button>
-                </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="4">Loading...</td>
-                </tr>
-            </tbody>
-        </table>
     </div>
     
 </template>
@@ -239,37 +63,28 @@
 <script>
 // import sourdata from '@/data.json' 
 import axios from 'axios';
-
+import DynamicTable from './../admin/dynamicTable/DynamicTable.vue'
 export default {
+    components:{
+        DynamicTable
+    },
     data(){
         return{
-            tables: null
+            tables: []
         }
     },
-    mounted(){
-        
+    mounted() {
         this.getTable();
-        
     },
     methods:{
         getTable() {
             let tablename = this.$route.params.table_name;
             axios.get(`http://localhost:3000/table`).then((res) => {
-                console.log(res.data[1][tablename])
-                if(tablename == "movies"){
-                    this.tables = res.data[0][tablename];
-                }else if(tablename == "users"){
-                    this.tables = res.data[1][tablename];
-                }else if(tablename == "mylists"){
-                    this.tables = res.data[2][tablename];
-                }else if(tablename == "actors"){
-                    this.tables = res.data[3][tablename];
-                }else if(tablename == "countries"){
-                    this.tables = res.data[4][tablename];
-                }else if(tablename == "genres"){
-                    this.tables = res.data[5][tablename];
-                }else if(tablename == "comments"){
-                    this.tables = res.data[6][tablename];
+                for(let i = 0; i < res.data.length; i++) {
+                    if(res.data[i][tablename]) {
+                        this.tables = res.data[i][tablename];
+                        break; 
+                    }
                 }
             }).catch(error => {
                 console.error(error);
@@ -328,23 +143,6 @@ export default {
     table tr {
         border-bottom: 0.5px solid rgb(193, 190, 190);
     }
-    .btn{
-        width: 100%;
-        height: 65px;
-        display: flex;
-        justify-content: end;
-        align-items: start;
-    }
-    .btn button{
-        height: 40px;
-        width: 70px;
-        margin: 5px;
-        background-color: rgb(11, 175, 11);
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        color: aliceblue;
-    }
     .edit-button{
         height: 40px;
         width: 70px;
@@ -366,3 +164,6 @@ export default {
         color: aliceblue;
     }
 </style>
+
+
+
---
+++ Movie_Nodejs_Vuejs/view/src/pages/admin/dynamicTable/DynamicTable.vue
@@ -0,0 +1,70 @@
+<template>
+    <table v-if="$route.params.table_name === entityType">
+      <thead>
+        <tr>
+          <th v-for="header in headers" :key="header">{{ header }}</th>
+          <th>Activity</th>
+        </tr>
+      </thead>
+  
+      <tbody v-if="data.length">
+        <tr v-for="item in data" :key="item.id">
+          <td v-for="property in headers" :key="property">{{ item[property.toLowerCase()] }}</td>
+          <td>
+            <button class="edit-button">Edit</button>
+            <button class="delete-button">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+  
+      <tbody v-else>
+        <tr>
+          <td :colspan="headers.length + 1">Loading...</td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <script>
+  export default {
+    name:'DynamicTable',
+    props: {
+      entityType: String,
+      headers: Array,
+      data: Array
+    }
+  };
+  </script>
+  
+  <style>
+  
+  table{
+        width: 98%;
+        height: 70%;
+        text-align: center;
+        border-collapse: collapse;
+    }
+    table tr {
+        border-bottom: 0.5px solid rgb(193, 190, 190);
+    }
+    .edit-button{
+        height: 40px;
+        width: 70px;
+        margin: 5px;
+        background-color: rgb(11, 90, 175);
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        color: aliceblue;
+    }
+    .delete-button{
+        height: 40px;
+        width: 70px;
+        margin: 5px;
+        background-color: rgb(195, 71, 49);
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        color: aliceblue;
+    }
+  </style>
